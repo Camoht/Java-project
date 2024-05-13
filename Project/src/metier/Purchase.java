ffac9d6c0bde957fcdc9d8a992af1c2e81c5c0d7
FranklinB13 <153076947+FranklinB13@users.noreply.github.com>
2024-05-13 15:35:31 +0200

    Update Purchase.java

--- Project/src/metier/Purchase.java
+++ Project/src/metier/Purchase.java
@@ -1,5 +1,36 @@
 package metier;
 
-public class Purchase {
+public class Achat {
+    private Film film;
+    private Date dateAchat;
+    private double montant;
 
-}
+    public Achat(Film film, Date dateAchat, double montant) {
+        this.film = film;
+        this.dateAchat = dateAchat;
+        this.montant = montant;
+    }
+
+    public Film getFilm() {
+        return film;
+    }
+
+    public void setFilm(Film film) {
+        this.film = film;
+    }
+
+    public Date getDateAchat() {
+        return dateAchat;
+    }
+
+    public void setDateAchat(Date dateAchat) {
+        this.dateAchat = dateAchat;
+    }
+
+    public double getMontant() {
+        return montant;
+    }
+
+    public void setMontant(double montant) {
+        this.montant = montant;
+    }
